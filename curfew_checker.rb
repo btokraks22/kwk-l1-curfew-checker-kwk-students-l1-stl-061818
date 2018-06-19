034d7ac596349f5a3623fb219a00068acb73b30c
btokraks22 <bree.v.v.13@gmail.com>
2018-06-19 19:53:08 +0000

    automatically backed up by learn

---
+++ curfew_checker.rb
@@ -0,0 +1,59 @@
+def curfew_checker(time)
+if 6 < 5
+  puts "Six is less than five."
+elsif 6 == 6 
+puts "six equals six."
+else 
+  puts "six is not less than five and six does not equal six."
+elsif 11 == 11
+puts "You're in trouble! Better get home quick!"
+else 
+  puts "You're in trouble! Better get home quick!"
+elsif 11 != 11
+puts "You're in troble! Better get home quick!"
+
+
+end
+
+curfew_checker(four)
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
